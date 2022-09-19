ba97c72bd63d4acb368d8ca737c63b39272f578d
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-09-19 17:54:59 +0600

    updated

--- src/main/java/pageObject/GolpoSholpo.java
+++ src/main/java/pageObject/GolpoSholpo.java
@@ -1,4 +1,13 @@
 package pageObject;
 
-public class GolpoSholpo {
+import driver.PageDriver;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Common;
+
+public class GolpoSholpo extends Common {
+    public GolpoSholpo(){
+        PageFactory.initElements(PageDriver.getCurrentDriver(),this);
+    }
+    //Locators
+    //Actions
 }
--- src/main/java/utilities/Common.java
+++ src/main/java/utilities/Common.java
@@ -1,4 +1,46 @@
 package utilities;
 
+import driver.PageDriver;
+import io.appium.java_client.MobileBy;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+
 public class Common {
+    /*******************************************
+     * *****************************************
+     * PassValue in the input field using sendTextMethod
+     * @param element
+     * @param value
+     */
+    // send text method sends values in a web element like inputs
+    public static void sendText(WebElement element, String value) {
+        element.clear();
+        element.sendKeys(value);
+    }
+    /*********************************
+     * *******************************
+     * Method---------3 Search by "text" scrolling top bottom
+     */
+
+    //with instance
+    public void androidScrollToAnElementByText(String text) {
+        try {
+            (PageDriver.getCurrentDriver()).findElement(
+                    MobileBy.AndroidUIAutomator(
+                            "new UiScrollable(new UiSelector().scrollable(true).instance(1)).scrollIntoView(new UiSelector().text(\"" + text + "\").instance(0))")).click();
+        } catch (Exception e) {
+            throw new NoSuchElementException("No element " + e);
+        }
+    }
+
+    //without instance
+    public void androidScrollByText(String text) {
+        try {
+            (PageDriver.getCurrentDriver()).findElement(
+                    MobileBy.AndroidUIAutomator(
+                            "new UiScrollable(new UiSelector().scrollable(true)).scrollIntoView(new UiSelector().text(\"" + text + "\").instance(0))")).click();
+        } catch (Exception e) {
+            throw new NoSuchElementException("No element " + e);
+        }
+    }
 }
