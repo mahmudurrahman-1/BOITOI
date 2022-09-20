f37d38c54d2225bf113b3550c6f76822904456ed
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-09-20 13:23:40 +0600

    update

--- src/main/java/pageObject/GolpoSholpo.java
+++ src/main/java/pageObject/GolpoSholpo.java
@@ -1,6 +1,8 @@
 package pageObject;
 
 import driver.PageDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Common;
 
@@ -9,5 +11,66 @@ public class GolpoSholpo extends Common {
         PageFactory.initElements(PageDriver.getCurrentDriver(),this);
     }
     //Locators
+    //sholpogolpo
+//goto second feature
+   @FindBy(id = "ridmik.boitoi:id/rlShortStory")
+    WebElement storyCon;
+
+  //  click write sholpo golpo
+    @FindBy(id = "ridmik.boitoi:id/viewWriteShortStory")
+    WebElement storyWriteViewBtn;
+
+    //click write new story
+    @FindBy(id = "ridmik.boitoi:id/fabWriteNewBook")
+    WebElement storyNewWriteBtn;
+
+
+//-enter
+    @FindBy(id = "ridmik.boitoi:id/tvEditBookName")
+    WebElement storyBookName;
+
+//-next
+    @FindBy(id = "ridmik.boitoi:id/tvNext")
+    WebElement storyNextBtn;
+
+//image upload
+//-upload
+    @FindBy(id = "ridmik.boitoi:id/tvUploadImage")
+    WebElement storyCoveruploadBtn;
+
+
+//--scroll
+    String imgTextSize= "1.41 MB";
+    String scrollConId = " com.android.documentsui:id/dir_list";
+
+    //  add new chapter click
+    @FindBy(id = "ridmik.boitoi:id/tvAddNewBookOnNoBook")
+    WebElement storyChapterAddBtn;
+
+    //enter or start writting
+    @FindBy(id = "ridmik.boitoi:id/storyEditorEditText")
+    WebElement storyInputText;
+
+    // bold you writting
+    @FindBy(id = "ridmik.boitoi:id/storyEditorButtonBold")
+    WebElement storyInputBoldBtn;
+
+//--save
+    @FindBy(id = "ridmik.boitoi:id/tvSaveChapter")
+    WebElement storySaveBtn;
+
+    //  publish click
+    @FindBy(id = "ridmik.boitoi:id/tvButtonThree")
+    WebElement storyCon;
+
+    //       click
+    //free-
+    @FindBy(id = "")
+    WebElement storyCon;
+    ridmik.boitoi:id/viewFree
+//--publish click
+    @FindBy(id = "")
+    WebElement storyCon;
+    ridmik.boitoi:id/tvPublish
     //Actions
 }
